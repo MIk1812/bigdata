2213a634ed06d51901df25afac954cbb697aad64
Mik1812 <s183913@student.dtu.dk>
2021-11-15 20:17:41 +0100

    Removal of outliers

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 import pandas as pd
-from LoadData import load_data
+from Preprocessing import load_data
 
 X = pd.read_csv("Data/feature_transform.csv", delimiter=',')
+X_no_outliers = pd.read_csv("Data/feature_transform_outliers_removed.csv", delimiter=',')
 Xk = pd.read_csv("Data/one_out_of_k.csv", delimiter=',')
+Xk_no_outliers = pd.read_csv("Data/one_out_of_k_outliers_removed.csv", delimiter=',')
+
+
