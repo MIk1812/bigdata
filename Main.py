ecf6d100ad1f5f64ea2131ea155d736bdbd93674
Mik1812 <s183913@student.dtu.dk>
2021-11-15 19:55:33 +0100

    New column order in data

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from LoadData import load_data
 
-X = pd.read_csv("Data/feature_transform.csv", delimiter=',')
-Xk = pd.read_csv("Data/one_out_of_k.csv", delimiter=',')
-
+X = pd.read_csv("Data/one_out_of_k.csv", delimiter=',')
+Xk = pd.read_csv("Data/feature_transform.csv", delimiter=',')
 
 
